7484476584f0b64d750024dac5d534192dd0565b
Nevin000 <nevinjeesara18@gmail.com>
2024-08-14 13:05:13 +0530

    confitee animation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
   const storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
   if (storedTasks) {
@@ -6,16 +6,15 @@ document.addEventListener("DOMContentLoaded", ()=> {
       tasks.push(task);
       updateTasksList();
       updateStats();
-    })
+    });
   }
-
-})
+});
 
 let tasks = [];
 
 const saveTasks = () => {
   localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+};
 
 const addTask = () => {
   const taskInput = document.getElementById("taskInput");
@@ -72,7 +71,13 @@ const updateStats = () => {
 
   progressBar.style.width = `${progress}%`;
 
-  document.getElementById("numbers").innerHTML = `${completedTasks} / ${totalTasks}`;
+  document.getElementById(
+    "numbers"
+  ).innerHTML = `${completedTasks} / ${totalTasks}`;
+
+  if (tasks.length && completedTasks === totalTasks) {
+    blaskConfetti();
+  }
 };
 
 const updateTasksList = () => {
@@ -105,3 +110,35 @@ document.getElementById("newTask").addEventListener("click", function (e) {
   e.preventDefault();
   addTask();
 });
+
+const blaskConfetti = () => {
+  const defaults = {
+    spread: 360,
+    ticks: 50,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["star"],
+    colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+  };
+
+  function shoot() {
+    confetti({
+      ...defaults,
+      particleCount: 40,
+      scalar: 1.2,
+      shapes: ["star"],
+    });
+
+    confetti({
+      ...defaults,
+      particleCount: 10,
+      scalar: 0.75,
+      shapes: ["circle"],
+    });
+  }
+
+  setTimeout(shoot, 0);
+  setTimeout(shoot, 100);
+  setTimeout(shoot, 200);
+};
